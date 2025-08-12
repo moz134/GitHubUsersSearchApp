375cc76a72c595492339796cb819c401e5adb96a
md mozammil <md.mozammil134@gmail.com>
2025-08-13 01:19:27 +0530

    core functionalites completed for github users search

---
+++ GitHubUserSearchApp/GitHubUserSearchApp/GitHubUser/GitHubUserViews/GitHubUserReposView.swift
@@ -0,0 +1,7 @@
+//
+//  GitHubUserRepos.swift
+//  GitHubUserSearchApp
+//
+//  Created by Md Mozammil on 13/08/25.
+//
+
---
+++ GitHubUserSearchApp/GitHubUserSearchApp/GitHubUserSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUserSearchAppApp.swift
+//  GitHubUserSearchApp
+//
+//  Created by Md Mozammil on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUserSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
